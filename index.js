bc355d8248c530f6d36d8c0017a60bc0da3dd837
Hassan Nurudeen <nurudeenhassan862@gmail.com>
2024-08-16 10:25:37 +0100

    fix: connection to MongoDB

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const cors = require('cors')
 const Note = require('./models/note')
+const cors = require('cors')
+
 
 app.use(express.json())
 app.use(cors())

a2dfdf480fc99c427855c537c98a3d7710249bc5
Hassan Nurudeen <nurudeenhassan862@gmail.com>
2024-08-17 16:24:01 +0100

    fix: fixess

--- index.js
+++ index.js
@@ -11,24 +11,6 @@ app.use(cors())
 
 const password = process.argv[2]
 
-let notes = [
-    {
-      id: "1",
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: "2",
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: "3",
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-  ]
-
   const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
     console.log('Path:  ', request.path)
